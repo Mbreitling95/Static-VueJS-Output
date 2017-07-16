64ebb1096cb693304d481f44e16d09c128372137
Matthew Breitling <MBreitling95@gmail.com>
2017-07-15 20:10:43 -0500

    Commit output

---
+++ js/main.js
@@ -0,0 +1,54 @@
+
+var test = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue.js!'
+  }
+});
+
+
+
+// new Vue({
+//   el: "#d",
+//   data: {
+//       name: "Matthew Breitling",
+//       role: "Front End Web Developer",
+//       welcomeMessage: "Welcome to my Bio page, please contact me if you want more information",
+//       bioPic: "../assets/Profile_Pic.jpg",
+//       test: "",
+//       skills: ["HTML & HTML5", "CSS & CSS3", 
+//         "Javascript", "JQuery ", "Knockout JS", "React JS (This site is made with React)"
+//       ],
+//       contacts: [
+//         ["Mobile", "903-588-4139"],
+//         ["Email", "Mbreitling95@gmail.com"],
+//         ["Github", "Mbreitling95"],
+//         ["StackOverflow", "Mbreitling95"],
+//         ["Location", "Winnsboro TX"]
+//       ]
+//     }
+// })
+
+// new Vue({
+//   el: "#static",
+//   data: {
+//     msg: "Test",
+//     bio: {
+//       name: "Matthew Breitling",
+//       role: "Front End Web Developer",
+//       welcomeMessage: "Welcome to my Bio page, please contact me if you want more information",
+//       bioPic: "../assets/Profile_Pic.jpg",
+//       test: "",
+//       skills: ["HTML & HTML5", "CSS & CSS3", 
+//         "Javascript", "JQuery ", "Knockout JS", "React JS (This site is made with React)"
+//       ],
+//       contacts: [
+//         ["Mobile", "903-588-4139"],
+//         ["Email", "Mbreitling95@gmail.com"],
+//         ["Github", "Mbreitling95"],
+//         ["StackOverflow", "Mbreitling95"],
+//         ["Location", "Winnsboro TX"]
+//       ]
+//     },
+//   }//dataObj
+// })
